80fc9d5856f84891dad04ae44a102707a91d7efb
Augustine von Trapp <augustvontrapp@gmail.com>
2018-11-29 15:58:35 -0800

    structure, importing p5, bundling, and basic functioning code. could be imported as 'library that build off of p5'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-import posPronounDecliner from './posPronounDecliner.js';
+import * as p5 from 'p5';
 
-export default {
-  posPronounDecliner
-};
+let s = (sk) => {
+
+  sk.setup = () => {
+    sk.createCanvas(720, 400);
+    sk.background(230);
+    sk.strokeWeight(2);
+  }
+
+  sk.draw = () => {
+    if (sk.mouseIsPressed){
+      sk.stroke(255);
+    }
+    else {
+      sk.stroke(237,34,93);
+    }
+    sk.line(sk.mouseX-66, sk.mouseY, sk.mouseX+66, sk.mouseY);
+    sk.line(sk.mouseX, sk.mouseY-66, sk.mouseX, sk.mouseY+66);
+  }
+}
+
+const P5 = new p5(s);
