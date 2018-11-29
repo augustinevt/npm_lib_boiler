15766204ea44f053e78fe1a74f49d535add6eb1d
august von trapp <augustvontrapp@gmail.com>
2018-11-28 18:21:29 -0800

    make yml jsonify and concatenate

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const fs = require('fs');
-const WATCH_TARGET = './node-desc.txt';
-fs.watch(WATCH_TARGET, (eventType, filename) => {
-   console.log('File "' + filename + '" was changed: ' + eventType);
+const yml = require('yamljs');
+
+const WATCH_TARGET = './input.yml';
+
+fs.watchFile(WATCH_TARGET, (eventType, filename) => {
+  const jsonOutput = yml.load('./input.yml');
+  let textOutput = '';
+
+  for(let sectionName in jsonOutput.content) {
+    console.log(sectionName)
+    const section = jsonOutput.content[sectionName];
+    textOutput +=
+`${section.intro} ${section.context} ${section.substantiation} ${section.analysis} ${section.conclusion}\n\n`
+  };
+
+  fs.writeFile('./output.json', JSON.stringify(jsonOutput), (err) => {
+    if (err) throw err;
+    console.log('json file saved!')
+  });
+
+  fs.writeFile('./output.txt', textOutput, (err) => {
+    if (err) throw err;
+    console.log('text file saved!')
+  });
 });
