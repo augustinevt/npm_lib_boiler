aee1ecae626c85e3247989d4e0fda0bcc6d9f369
august von trapp <augustvontrapp@gmail.com>
2018-12-03 21:44:47 -0800

    add generator

--- src/index.js
+++ src/index.js
@@ -3,21 +3,60 @@ const yml = require('yamljs');
 
 const WATCH_TARGET = './input.yml';
 
+const nouns = {};
+
+const addNoun = (opts) => {
+  console.log('adding noun with options:', opts)
+  nouns[opts.name] = opts;
+  return `${opts.name}`
+}
+
+const getNoun = (opts) => {
+  console.log('you retreived this noun!!', nouns[opts.name]);
+  return `${opts.name}`
+}
+
+const bank = {
+  addNoun,
+  getNoun
+}
+
+function* generator(i) {
+  yield i;
+  yield i + 10;
+  return "food!"
+}
+
 fs.watchFile(WATCH_TARGET, (eventType, filename) => {
   const jsonOutput = yml.load('./input.yml');
-  let textOutput = '';
+  let gen = null;
+  let textOutput;
 
-  for(let sectionName in jsonOutput.content) {
-    console.log(sectionName)
-    const section = jsonOutput.content[sectionName];
-    textOutput +=
-`${section.intro} ${section.context} ${section.substantiation} ${section.analysis} ${section.conclusion}\n\n`
-  };
+  const cmd = jsonOutput.cmd;
+  const opts = jsonOutput.opts;
 
-  fs.writeFile('./output.json', JSON.stringify(jsonOutput), (err) => {
-    if (err) throw err;
-    console.log('json file saved!')
-  });
+  if (cmd === 'gen') {
+    gen = generator(10);
+  }
+
+  console.log(gen)
+
+  try {
+    if (!!gen) {
+
+      // don't CALL NEXT!! HAHA
+      textOutput = gen.next()
+      if (gen.next.done) {
+        gen = null;
+      }
+
+    } else {
+      textOutput = bank[cmd](opts);
+    }
+
+  } catch (e) {
+    textOutput =`\n\n -------- \n ${e}`
+  }
 
   fs.writeFile('./output.txt', textOutput, (err) => {
     if (err) throw err;
